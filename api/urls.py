f918e220a1afa2a0c9f52637e6f0baf61103e097
aniket <ani02ket@gmail.com>
2023-03-02 15:10:42 +0530

    Initial commit

--- api/urls.py
+++ api/urls.py
@@ -5,9 +5,5 @@ from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path("register/", RegistrationView.as_view(), name="register"),
     path("verify/",VerifyOTP.as_view()),
-    path("otp/",GenerateOTP.as_view()),
-    # path("login/", LoginView.as_view(), name="login"),
-    # path('gettoken/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('refreshtoken/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    # path("update/<str:pk>",Updateview.as_view(),name="update"),
+    path("otp/",GenerateOTP.as_view())
 ]
